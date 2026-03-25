b8c49807139f0252b47701463b495de46c35c5fa
so0 <137738994+soyoung1130@users.noreply.github.com>
2026-03-25 19:59:31 +0900

    123

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello test!~~")
+		fmt.Fprintln(w, "hello test!123")
 	})
 	fmt.Println("Server started at :8080")
 	http.ListenAndServe(":8080", nil)
